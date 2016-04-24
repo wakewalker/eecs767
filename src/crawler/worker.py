fb60a4efa1765a563ed6bbfee4e5c73eda4b028e
Ubuntu <ubuntu@ip-172-31-26-89.us-west-2.compute.internal>
2016-04-24 17:51:28 +0000

    Workers now setup to use config.

--- src/crawler/worker.py
+++ src/crawler/worker.py
@@ -8,35 +8,48 @@ from tokenizer import DocProcessor
 from indexer import DocList, InvertedIndex
 
 
-def crawl(url, delay=1):
+def crawl(url, config):
     '''
     RQ worker function which extracts URLs from the page contents at given the
     URL, then passes new URLs to both the CRAWL and PROCESS queues for futher
     action.
     '''
+
+    DELAY = config.get('crawler', 'crawl_delay')
+    MAX_DOCS = config.get('crawler', 'max_docs')
+
+    sleep(DELAY)
+
     wc = WebCrawler()
     urls = wc.crawl(url)
-
-    sleep(delay)
     
     dl = DocList()
-    if len(dl) < 1000:
+    if len(dl) < MAX_DOCS:
         redis_conn = Redis()
         for url in urls:
             did = md5(url).hexdigest()
             if did not in dl:
                 pq = Queue('crawl', connection=redis_conn)
-                pq.enqueue(crawl, url);
+                wq.enqueue(crawl, args=(
+                    url,
+                    config
+                ));
                 pq = Queue('process', connection=redis_conn)
-                pq.enqueue(process, url);
+                wq.enqueue(process, args=(
+                    url,
+                    config
+                ));
 
-def process(url):
+
+def process(url, config):
     '''
     RQ worker function which tokenizes the page contents at the given URL, then 
     passes on the document posting list to the WRITE queue for futher action.
     '''
+    MAX_DOCS = config.get('crawler', 'max_docs')
+    
     dl = DocList()
-    if len(dl) < 1000:
+    if len(dl) < MAX_DOCS:
         did = md5(url).hexdigest()
         if did not in dl:
             dproc = DocProcessor()
@@ -47,22 +60,31 @@ def process(url):
             wq = Queue('write', connection=redis_conn)
             wq.enqueue(write, args=(
                 plist,
-                url
+                url,
+                config
             ));
 
-def write(plist, url):
+
+def write(plist, url, config):
     '''
     RQ worker function which adds the given document posting list data to the
     inverted index.
     '''
+    MAX_DOCS = config.get('crawler', 'max_docs')
+    TERM_DICT_FILE = config.get('indexer', 'term_dict_file')
+    DOC_LIST_FILE = config.get('indexer', 'term_dict_file')
+    
     dl = DocList()
-    if len(dl) < 1000:
+    if len(dl) < MAX_DOCS:
         did = md5(url).hexdigest()
 
         if did not in dl:
             dl.append(url)
 
-            iidx = InvertedIndex()
+            iidx = InvertedIndex(
+                TERM_DICT_FILE,
+                DOC_LIST_FILE
+            )
             iidx.append(plist, did)
             iidx.update()
 

435d43b6ce7905185bf21128f97ee6e638509717
Ubuntu <ubuntu@ip-172-31-26-89.us-west-2.compute.internal>
2016-04-24 17:34:39 +0000

    Updated query test.

--- bin/tests/query.py
+++ bin/tests/query.py
@@ -1,8 +1,14 @@
 from operator import itemgetter
 
 from indexer import InvertedIndex
+from tokenizer import DocProcessor
+
+query = 'asian women alumni'
+query = 'mary had a little lamb whos fleece was white as snow'
+dproc = DocProcessor()
+dproc.prep_query(query)
 iidx = InvertedIndex()
-rel_docs = iidx.query(['asian', 'women', 'alumni'])
+rel_docs = iidx.query(dproc.tokens)
 
 ranked_docs = sorted(rel_docs.items(), key=itemgetter(1), reverse=True)
 

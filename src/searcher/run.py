b79f783730d244cb79c4b0c1cba73a421a995ad4
Ubuntu <ubuntu@ip-172-31-26-89.us-west-2.compute.internal>
2016-04-30 19:01:34 +0000

    Got web UI mostly working.

--- src/searcher/run.py
+++ src/searcher/run.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from operator import itemgetter
+from flask import Flask, render_template, request
+
+from tokenizer import DocProcessor
+from indexer import InvertedIndex, DocList
+
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-    #    return 'Searching all the things!'
+    if request.method == 'GET':
+        return render_template('index.html')#, request=request)
+    else:
+        query = request.form['query']
+
+        dproc = DocProcessor()
+        dproc.prep_query(query)
+        
+        iidx = InvertedIndex(
+            '/home/ubuntu/eecs767/var/ku/term.dct',
+            '/home/ubuntu/eecs767/var/ku/doc.lst'
+        )
+        rel_docs = iidx.query(dproc.tokens)
+        ranked_docs = sorted(rel_docs.items(), key=itemgetter(1), reverse=True)
+
+        dlist = DocList(
+            '/home/ubuntu/eecs767/var/ku/doc.lst'
+        )
+
+        abstract = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae purus sit amet magna iaculis rhoncus. Aenean ullamcorper nibh vitae lacus commodo condimentum. Aenean ornare pharetra est id porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu ante sed arcu maximus imperdiet. Phasellus id nisl quis sem consectetur sagittis. Duis placerat nisi ut nisl condimentum ornare. Sed pulvinar arcu nisl, eu faucibus dui tincidunt aliquam. Aliquam malesuada faucibus nisl, et malesuada turpis sagittis nec. Aliquam id pretium augue.'
+
+        results = {}
+        for doc in ranked_docs[:10]:
+            results[doc[0]] = {
+                'url': dlist[doc[0]],
+                'score': doc[1],
+                'abstract': abstract
+            }
+
+        return render_template('index.html', 
+            #request=request,
+            #tokens=dproc.tokens,
+            #docs=ranked_docs[:10],
+            query=query,
+            results=results 
+        )
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
+

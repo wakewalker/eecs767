1bfc3d078d1758378718d240be2d1e74ce23faef
Ubuntu <ubuntu@ip-172-31-26-89.us-west-2.compute.internal>
2016-04-30 16:43:54 +0000

    Landing page mockup.

--- src/searcher/run.py
+++ src/searcher/run.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Searching all the things!'
+    return render_template('index.html')
+    #    return 'Searching all the things!'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
